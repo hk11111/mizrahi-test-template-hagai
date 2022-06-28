5c1c0f0690f6be007a7725925bddaf5165a647b8
Hagai Klugman <hk111112222233333@gmail.com>
2022-06-28 14:19:39 +0300

    initial

---
+++ src/app/table/table.component.ts
@@ -0,0 +1,47 @@
+import { User } from '../user';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+declare var require: any;
+var data: User[] = require('../data/MOCK_DATA.json');
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css'],
+})
+export class TableComponent implements AfterViewInit {
+  displayedColumns: string[] = [
+    'id',
+    'account_id',
+    'first_name',
+    'last_name',
+    'email',
+  ];
+  dataSource = new MatTableDataSource(data);
+
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  constructor(private _liveAnnouncer: LiveAnnouncer) {}
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+}
